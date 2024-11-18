a2a64fdb81ca9ed9a7f8d95298639847fcccdbd7
Chinmay Shrikhande <62685653+Chinmay2001@users.noreply.github.com>
2024-11-18 15:27:51 -0500

    modified:   test/ener_vs_thre.png 	modified:   test/temp_test_eagle.jl 	deleted:    test/temp_zne.jl 	test/ener_vs_thre_init.png 	test/ener_vs_thre_init_127.png 	test/zne_thres.jl

--- test/temp_test_eagle.jl
+++ test/temp_test_eagle.jl
@@ -13,11 +13,11 @@ function run_temp(N)
     o = Pauli(N, X=[14,30,32], Y=[10,31], Z=[9,13,18,29,33])
     # i = 8
     # for o in op
-    for i in 0:2:16
+    for i in 0:2:2
         # α = 0.3
         α = i * π/32
-        generators, parameters = UnitaryPruning.eagle_processor(o, α=α, k=20)
-        # println(length(generators))
+        generators, parameters = UnitaryPruning.eagle_processor(o, α=α, k=1)
+        println(generators)
         # e = UnitaryPruning.bfs_evolution(generators, parameters, o, ket, thres=1e-4)
         e , nops, c_norm2 = UnitaryPruning.bfs_evolution(generators, parameters, PauliSum(o), ket, thresh=1e-3)
 
