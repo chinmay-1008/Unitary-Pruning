089bbe076fb78a4814e0fa9a764ff5827a081a78
Chinmay Shrikhande <62685653+Chinmay2001@users.noreply.github.com>
2024-12-10 13:49:04 -0500

    renamed:    test/ener_vs_Jx.pdf -> test/all_data/ener_vs_Jx.pdf 	renamed:    test/ener_vs_Jx_127_f.pdf -> test/all_data/ener_vs_Jx_127_f.pdf 	renamed:    test/ener_vs_Jx_127_t.pdf -> test/all_data/ener_vs_Jx_127_t.pdf 	renamed:    test/ener_vs_Jx_8_f.pdf -> test/all_data/ener_vs_Jx_8_f.pdf 	renamed:    test/ener_vs_Jx_8_f_det.pdf -> test/all_data/ener_vs_Jx_8_f_det.pdf 	renamed:    test/ener_vs_Jx_8_f_loose.pdf -> test/all_data/ener_vs_Jx_8_f_loose.pdf 	renamed:    test/sam1000_ev_N8_Jx.dat -> test/all_data/sam1000_ev_N8_Jx.dat 	renamed:    test/sam1000_ev_N8_Jx_exact.dat -> test/all_data/sam1000_ev_N8_Jx_exact.dat 	renamed:    test/sam1000_ev_layer_f_N10.dat -> test/all_data/sam1000_ev_layer_f_N10.dat 	renamed:    test/sam1000_ev_layer_t_N10.dat -> test/all_data/sam1000_ev_layer_t_N10.dat 	renamed:    test/sam1000_ev_t_N10_tight.dat -> test/all_data/sam1000_ev_t_N10_tight.dat 	new file:   test/all_data/sam100_ev_f_N10_nor.dat 	new file:   test/all_data/sam100_ev_f_N10_nor_ren.dat 	new file:   test/all_data/sam100_ev_f_N10_nor_ren_tight.dat 	new file:   test/all_data/sam100_ev_f_N10_uni.dat 	new file:   test/all_data/sam100_ev_f_N10_uni_mag.dat 	new file:   test/all_data/sam100_ev_f_N10_uni_ren.dat 	new file:   test/all_data/sam100_ev_f_N10_uni_ren_large.dat 	renamed:    test/sam100_ev_layer_t_N127.dat -> test/all_data/sam100_ev_layer_t_N127.dat 	renamed:    test/sam100_ev_nops_N8.dat -> test/all_data/sam100_ev_nops_N8.dat 	new file:   test/all_data/sam100_ev_t_N10_nor_ren_tight.dat 	renamed:    test/sam100_ev_t_N10_tight.dat -> test/all_data/sam100_ev_t_N10_tight.dat 	new file:   test/all_data/sam100_ev_t_N10_uni_ren.dat 	new file:   test/all_data/sam100_ev_t_N10_uni_ren_tight.dat 	new file:   test/all_data/sam100_ev_t_N10_uniform.dat 	new file:   test/all_data/sam100_ev_t_N10_uniform_tight.dat 	renamed:    test/sam_ev_layer_f_N50_.dat -> test/all_data/sam_ev_layer_f_N50_.dat 	renamed:    test/sam_ev_layer_t_N50_.dat -> test/all_data/sam_ev_layer_t_N50_.dat 	renamed:    test/sampled_ev_N50.dat -> test/all_data/sampled_ev_N50.dat 	deleted:    test/ener_vs_Jx_127.pdf 	deleted:    test/ener_vs_sigma_false.pdf 	deleted:    test/ener_vs_sigma_true.pdf 	modified:   test/kill_pauli.jl 	deleted:    test/sam100_ev_t_N10_renorm.dat

--- test/kill_pauli.jl
+++ test/kill_pauli.jl
@@ -18,8 +18,8 @@ function run(; N=10, k=5,sigma = 1e-3, layer = false)
     generators, parameters = UnitaryPruning.heisenberg(o, Jx = α, Jy = α,Jz = α, k=k)
     # ei_n , nops_n, c_norm2_n = UnitaryPruning.stochastic_bfs(generators, parameters, PauliSum(o), ket, sigma = sigma, layer = layer)
     # ei_n , nops_n, c_norm2_n = UnitaryPruning.flat_prob_kill_bfs(generators, parameters, PauliSum(o), ket, threshold = sigma)
-    # ei_n , nops_n, c_norm2_n = UnitaryPruning.stochastic_bfs_flat(generators, parameters, PauliSum(o), ket, sigma = sigma, layer = layer)
-    ei_n , nops_n, c_norm2_n = UnitaryPruning.stochastic_bfs_renormalized(generators, parameters, PauliSum(o), ket, sigma = sigma, layer = layer)
+    ei_n , nops_n, c_norm2_n = UnitaryPruning.stochastic_bfs_flat(generators, parameters, PauliSum(o), ket, sigma = sigma, layer = layer)
+    # ei_n , nops_n, c_norm2_n = UnitaryPruning.stochastic_bfs_renormalized(generators, parameters, PauliSum(o), ket, sigma = sigma, layer = layer)
  
 
     @printf("α: %6.4f e: %12.8f+%12.8fi nops: %6i norm2: %3.8f sigma: %3.10f\n", α, real(ei_n), imag(ei_n), maximum(nops_n), c_norm2_n, sigma)
@@ -34,7 +34,14 @@ end
 
 
 function run_temp()
-    stds = [i*1e-3 for i in 1:20]
+    st = 1e-4
+    stds = [i*st for i in 1:100]
+    # temp_std = [i*st for i in 30:10:100]
+    # for j in temp_std
+    #     push!(stds, j)
+    # end
+    # println(stds)
+
     energy = []
     for sigma in stds
 
@@ -62,7 +69,7 @@ function run_samples()
         push!(energies, temp_energies)
     end
 
-    writedlm("test/sam100_ev_t_N10_renorm.dat", energies)
+    writedlm("test/all_data/sam100_ev_f_N10_uni_large.dat", energies)
 
     return
 end
