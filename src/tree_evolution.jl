4de47fbad02038cf1d8b460ee648d3740e60a81b
Shrikhande Chinmay <chinmays@vt.edu>
2025-02-06 15:58:02 -0500

    modified:   .DS_Store 	modified:   src/tree_evolution.jl 	new file:   test/new_diff_N15-paper-0.25-20-0.001-0.25.pdf 	new file:   test/new_diff_N9-paper-0.25-20-0.001-0.03.pdf 	modified:   test/tree.jl 	new file:   test/tree_otoc.pdf 	new file:   test/tree_otoc10-0.001.pdf 	new file:   test/tree_otoc5-0.001.pdf

--- src/tree_evolution.jl
+++ src/tree_evolution.jl
@@ -66,3 +66,68 @@ function tree_evolution(generators::Vector{Pauli{N}}, angles, o::PauliSum{N}, ke
 
     return expval, n_ops, coeff_norm2, dicts
 end
+
+
+function tree_evolution_otoc(generators::Vector{Pauli{N}}, angles, o::PauliSum{N}, ket ; thresh=1e-3) where {N}
+
+    #
+    # for a single pauli Unitary, U = exp(-i θn Pn/2)
+    # U' O U = cos(θ) O + i sin(θ) OP
+    nt = length(angles)
+    length(angles) == nt || throw(DimensionMismatch)
+    vcos = cos.(angles)
+    vsin = sin.(angles)
+
+    # collect our results here...
+    step_evo = Int64(nt/(3*(N-1)))
+    otoc = Vector{Float64}(undef, step_evo)
+    temp_o = PauliSum(N)
+    time = 1
+    o_transformed = deepcopy(o)
+    dicts = Vector(undef, nt)    
+    sin_branch = PauliSum(N)
+ 
+    
+    for t in 1:nt
+
+        g = generators[t]
+
+        sin_branch = PauliSum(N)
+
+        for (oi,coeff) in o_transformed.ops
+           
+            abs(coeff) > thresh || continue
+
+
+            if commute(oi, g.pauli) == false
+                
+                # cos branch
+                o_transformed[oi] = coeff * vcos[t]
+
+                # sin branch
+                oj = g * oi    # multiply the pauli's
+                sum!(sin_branch, oj * vsin[t] * coeff * 1im)
+  
+            end
+        end
+        sum!(o_transformed, sin_branch) 
+        clip!(o_transformed, thresh=thresh)
+        # println((n_ops[t]))
+        # println(o_transformed)
+        dicts[t] = deepcopy(o_transformed)
+        println("Generator: ", t)
+        if t%(3*(N-1)) == 0
+            println(time)
+            os = o*o_transformed
+            println("First otoc done")
+            # display(os)
+            temp_o = os*os
+            println("Second otoc done")
+            otoc[time] = real(expectation_value(temp_o, ket))
+            time += 1
+        end
+
+    end
+
+    return otoc, dicts
+end
--- test/tree.jl
+++ test/tree.jl
@@ -11,18 +11,14 @@ function run(; N=10, k=5,threshold = 1e-3)
 
     i = 4
     α = i * π / 32 
-    generators, parameters = UnitaryPruning.heisenberg(o, Jx = 0.8, Jy = 0.9,Jz = 0.9, k=k)
-    ei_n , nops_n, c_norm2_n, dict = UnitaryPruning.tree_evolution(generators, parameters, PauliSum(o), ket, thresh = threshold)
+    generators, parameters = UnitaryPruning.heisenberg(o, Jx = 0.8/2, Jy = 0.9/2,Jz = 0.9/2, k=k)
+    # ei_n , nops_n, c_norm2_n, dict = UnitaryPruning.tree_evolution(generators, parameters, PauliSum(o), ket, thresh = threshold)
+    otoc, dict = UnitaryPruning.tree_evolution_otoc(generators, parameters, PauliSum(o), ket, thresh = threshold)
  
 
-    @printf("α: %6.4f e: %12.8f+%12.8fi nops: %6i norm2: %3.8f threshold: %3.10f\n", α, real(ei_n), imag(ei_n), maximum(nops_n), c_norm2_n, threshold)
+    # @printf("α: %6.4f e: %12.8f+%12.8fi nops: %6i norm2: %3.8f threshold: %3.10f\n", α, real(ei_n), imag(ei_n), maximum(nops_n), c_norm2_n, threshold)
 
-    # U = UnitaryPruning.build_time_evolution_matrix(generators, parameters)
-    # o_mat = Matrix(o)
-    # m = diag(U'*o_mat*U)
-    # println(real(m[1]), " ", real(ei_n), " Error: ",real(m[1]) -  real(ei_n))
-    # display(dict)
-    # pauli_evol = []
+    println(otoc)
     evol = []
     for data in dict
         temp_pauli = []
@@ -34,12 +30,18 @@ function run(; N=10, k=5,threshold = 1e-3)
         push!(evol, temp_pauli)
         push!(evol, temp_coeff)
     end
-    writedlm("test/tree_evolN$N-$threshold.dat", evol)
+    time_steps = [ i for i in 1:k]
+    plot(time_steps, otoc)
+    xlabel!("Time")
+    ylabel!("OTOC")
+    title!("N=$N; thresh = $threshold")
+    savefig("test/tree_otoc$N-$threshold.pdf")
+ 
+    # writedlm("test/tree_evolN$N-$threshold.dat", evol)
     return
-    # println(dict[1])
 end
 
-run(N = 5, k = 5, threshold = 0)
+@time run(N = 10, k = 20, threshold = 1e-3)
 # a = Pauli(6)
 # display(a)
 # println(string(a))
