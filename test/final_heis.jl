148f898b5a5160b6a6a215912e12f47c52b7fca1
Shrikhande Chinmay <chinmays@vt.edu>
2025-06-23 16:15:57 -0400

    modified:   test/final_heis.jl 	new file:   test/hubbard_1d_weights_L3-k10-th-1.pdf 	new file:   test/hubbard_1d_weights_L4-k10-th-1.pdf 	new file:   test/hubbard_1d_weights_L5-k10-th-1.pdf 	new file:   test/hubbard_1d_weights_L5-k10-th0.0001-2.pdf 	new file:   test/hubbard_1d_weights_L5-k10-th0.001-2.pdf

--- test/final_heis.jl
+++ test/final_heis.jl
@@ -15,10 +15,10 @@ function run(; N=10, k=5, thresh=1e-3, w_type = 0, w = 2)
     ket = KetBitString(N, 0) 
     o = Pauli(N, Z=[1])
 
-    generators, parameters = UnitaryPruning.heisenberg(o, Jx = 0.8, Jy = 0.9,Jz = 0.9, k=k)
+    # generators, parameters = UnitaryPruning.heisenberg(o, Jx = 0.8, Jy = 0.9,Jz = 0.9, k=k)
     # generators, parameters = UnitaryPruning.heisenberg(o, Jx =1.0, Jy = 1.0,Jz = 1.0, k=k)
     # generators, parameters = UnitaryPruning.heisenberg_2D(o, Jx = 0.8, Jy = 0.9,Jz = 0.9, k=k)
-    # generators, parameters = UnitaryPruning.fermi_hubbard_1D(o, t = 0.8, U = 4, k=k)
+    generators, parameters = UnitaryPruning.fermi_hubbard_1D(o, t = 1, U = 8, k=k)
 
     ei , nops, c_norm2 = UnitaryPruning.bfs_evolution_weight(generators, parameters, PauliSum(o), ket, thresh=thresh, w_type = w_type, w = w)
     
@@ -26,41 +26,42 @@ function run(; N=10, k=5, thresh=1e-3, w_type = 0, w = 2)
 
     @printf(" α: %6.4f e: %12.8f+%12.8fi nops: %6i norm2: %3.8f threshold: %3.10f\n", α, real(ei), imag(ei), maximum(nops), c_norm2, thresh)
 
-    U = UnitaryPruning.build_time_evolution_matrix_fast(generators, parameters)
+    # U = UnitaryPruning.build_time_evolution_matrix_fast(generators, parameters)
     # o_mat = Matrix(o)
     # m = diag(U'*o_mat*U)
     # error = real(m[1]) -  real(ei) 
     # println(real(m[1]), " ", real(ei), " Error: ", error)
 
 
-    ψ = zeros(ComplexF64, 2^N); ψ[1] = 1.0
-    ψp = U * ψ
-    e = dot(ψp, Matrix(o) * ψp)
-    error = real(e) -  real(ei) 
-    println(real(e), " ", real(ei), " Error: ", error)
+    # ψ = zeros(ComplexF64, 2^N); ψ[1] = 1.0
+    # ψp = U * ψ
+    # e = dot(ψp, Matrix(o) * ψp)
+    # error = real(e) -  real(ei) 
+    # println(real(e), " ", real(ei), " Error: ", error)
 
     return real(ei)
 end
 
 function run_weights()
-    L = 4
+    L = 5
 
     k = 10
-    N = L * L
+    N = 2 * L 
     o = Pauli(N, Z=[1])
     thresh = 1e-4
     # generators, parameters = UnitaryPruning.heisenberg(o, Jx = 0.8, Jy = 0.9,Jz = 0.9, k=k)
     # generators, parameters = UnitaryPruning.heisenberg(o, Jx =1.0, Jy = 1.0,Jz = 1.0, k=k)
-    generators, parameters = UnitaryPruning.heisenberg_2D(o, Jx = 0.8, Jy = 0.9,Jz = 0.9, k=k)
-    # generators, parameters = UnitaryPruning.fermi_hubbard_1D(o, t = 0.8, U = 4, k=k)
+    # generators, parameters = UnitaryPruning.heisenberg_2D(o, Jx = 0.8, Jy = 0.9,Jz = 0.9, k=k)
+    generators, parameters = UnitaryPruning.fermi_hubbard_1D(o, t = 1 , U = 8, k=k)
 
 
     # U = UnitaryPruning.build_time_evolution_matrix(generators, parameters)
     # o_mat = Matrix(o)
     # m = diag(U'*o_mat*U)
-    # println("Exact Expectaition Value: ", m[1])
+    m = [0]
+    println("Exact Expectaition Value: ", m[1])
 
-    weights = [i for i in 1:N]
+    weights = [i for i in 1:2*N]
 
     w_type = 0
     p_errors = []
@@ -68,8 +69,8 @@ function run_weights()
     for i in weights
         println("Weight Threshold: ", i)
         e_v = run(N = N, k = k, thresh = thresh, w_type = w_type, w = i)
-        # push!(p_errors, abs(real(m[1]) - real(e_v)))
-        push!(p_errors, real(e_v))
+        push!(p_errors, abs(real(m[1]) - real(e_v)))
+        # push!(p_errors, real(e_v))
 
     end
     display(p_errors)
@@ -97,8 +98,8 @@ function run_weights()
     for i in weights
         println("Weight Threshold: ", i)
         e_v = run(N = N, k = k, thresh = thresh, w_type = w_type, w = i)
-        # push!(m_errors, abs(real(m[1]) - real(e_v)))
-        push!(m_errors, real(e_v))
+        push!(m_errors, abs(real(m[1]) - real(e_v)))
+        # push!(m_errors, real(e_v))
 
     end
     # display(errors)
@@ -119,12 +120,12 @@ function run_weights()
         legend = :topright,
         grid = true)
 
-    savefig("test/heisenber_2d_weights_L$L-k$k-th$thresh.pdf")
+    savefig("test/hubbard_1d_weights_L$L-k$k-th$thresh-2.pdf")
 
 end
 
-# run_weights()
-@time run(N = 15, k = 10, thresh = -1, w_type = 0, w=8)
+run_weights()
+# @time run(N = 10, k = 10, thresh = -1, w_type = 0, w=8)
 # N = 2
 # N = 2*N
 # k = 1
